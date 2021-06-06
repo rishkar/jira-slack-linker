1e6824a14a7b1c308ddd3da66ef28dbc7d651770
Rishabh Karanth <me@karanth.dev>
2021-06-06 19:26:42 -0400

    Add extra preflight checks for conf file

--- jira_slack_bot.py
+++ jira_slack_bot.py
@@ -1,14 +1,40 @@
 import os
 import sys
-import time
-import re
+import configparser
 
 def do_preflight_checks():
+    # Make sure our slackbot.conf and all its required values exist
     if not os.path.isfile('slackbot.conf'):
         gen_conf_file()
         print("A configuration file has been generated at \'./slackbot.conf\'."
-                "Please fill this out and start the bot again.")
+            " Please fill this out and start the bot again.")
         sys.exit(0)
+    else:
+        conf_parser = configparser.ConfigParser()
+        conf_parser.read('slackbot.conf')
+
+        if not ('SLACK_CONF' in conf_parser or 'JIRA_CONF' in conf_parser):
+            error("Invalid slackbot.conf. Either fix the errors, or delete"
+                " the file and let this program regenerate it.")
+        
+        if not (conf_parser['SLACK_CONF']['SLACK_BOT_TOKEN']:
+            error("Missing SLACK_BOT_TOKEN value in slackbot.conf. Please"
+                " remediate and restart this program.")
+        if not (conf_parser['SLACK_CONF']['SLACK_SIGN_SECRET']:
+            error("Missing SLACK_SIGN_SECRET value in slackbot.conf. Please"
+                " remediate and restart this program.")
+        if not (conf_parser['JIRA_CONF']['JIRA_URL']:
+            error("Missing JIRA_URL_TOKEN value in slackbot.conf. Please"
+                " remediate and restart this program.")
+        if not (conf_parser['JIRA_CONF']['JIRA_PREFIXES']:
+            warning("Missing SLACK_BOT_TOKEN value in slackbot.conf. I will"
+                " default to \"/([A-Z]+)-([0-9]+)/g\"")
+
+
+        
+
+
+
 
 def gen_conf_file():
     new_conf = open("slackbot.conf", "w")
