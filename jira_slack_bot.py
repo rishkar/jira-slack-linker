3d18ca5e2756734420a7938582048379502d3090
Rishabh Karanth <me@karanth.dev>
2021-06-06 15:27:17 -0400

    Add preflight checks

--- jira_slack_bot.py
+++ jira_slack_bot.py
@@ -1,5 +1,12 @@
 import os
+import sys
 import time
 import re
-from slackclient import SlackClient
+from slack import SlackClient
 
+def do_preflight_checks():
+    if not os.path.isfile('slackbot.conf'):
+        gen_conf_file()
+        print("A configuration file has been generated at \'./slackbot.conf\'."
+                "Please fill this out and start the bot again.")
+        sys.exit(0)
