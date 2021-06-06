49d7c609d2ab9d018c9e236d91086df94160e81d
Rishabh Karanth <me@karanth.dev>
2021-06-06 15:53:30 -0400

    Add gen_conf_file

--- jira_slack_bot.py
+++ jira_slack_bot.py
@@ -2,7 +2,6 @@ import os
 import sys
 import time
 import re
-from slack import SlackClient
 
 def do_preflight_checks():
     if not os.path.isfile('slackbot.conf'):
@@ -10,3 +9,9 @@ def do_preflight_checks():
         print("A configuration file has been generated at \'./slackbot.conf\'."
                 "Please fill this out and start the bot again.")
         sys.exit(0)
+
+def gen_conf_file():
+    new_conf = open("slackbot.conf", "w")
+    new_conf.write("[SLACK_CONF]\nSLACK_BOT_TOKEN=\"\"\n" \
+                    "SLACK_SIGN_SECRET=\"\"\n\n" \
+                    "[JIRA_CONF]\nJIRA_URL=\"\"\nJIRA_PREFIXES=\"\"\n")
